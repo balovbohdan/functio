afb13ad17fedbee12975c63c28b113b719eec642
balovbohdan <balovbohdan@gmail.com>
2021-02-25 23:11:35 +0200

    feat(create-maybe-util): self review fixes

--- src/utils/maybe/maybe.ts
+++ src/utils/maybe/maybe.ts
@@ -2,7 +2,7 @@ import { Map } from 'types';
 import { curry } from 'utils';
 import { Maybe } from 'containers';
 
-// maybe :: b -> (a -> b) -> Maybe a-> b
+// maybe :: b -> (a -> b) -> Maybe a -> b
 
 const maybe = <V, R>(fallback: R, map: Map<V, R>, maybe: Maybe<V>): R => (
   maybe.isNothing ? fallback : map(maybe.value)
